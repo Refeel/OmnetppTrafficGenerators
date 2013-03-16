84c72f66ed64782aeec697bc4b1a5f9aa767ccf4
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2013-03-16 21:31:17 +0100

    + packet generator exponential + packet generator on-off

---
+++ src/ExponentialPacketGenerator.cc
@@ -0,0 +1,91 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "ExponentialPacketGenerator.h"
+
+namespace omnetpptrafficgenerators {
+
+ExponentialPacketGenerator::ExponentialPacketGenerator() {
+    // TODO Auto-generated constructor stub
+
+}
+
+ExponentialPacketGenerator::~ExponentialPacketGenerator() {
+    // TODO Auto-generated destructor stub
+}
+
+simtime_t ExponentialPacketGenerator::getDelay() {
+    simtime_t time = par("exponentialDelayTime");
+    return time;
+}
+
+
+SimplePacket *ExponentialPacketGenerator::generatePacket() {
+
+    int src = getIndex();
+    int n = size();
+    int dest = intuniform(0,n-2);
+    if (dest>=src) dest++;
+
+    char msgname[20];
+    sprintf(msgname, "tic-%d-to-%d", src, dest);
+
+    SimplePacket *sp = new SimplePacket(msgname);
+
+    sp->setDST(dest);
+    sp->setSRC(src);
+    sp->setSessionId(0);
+    sp->setPacketId(this->_packetsCount++);
+    sp->setPriority(NORMAL);
+
+    return sp;
+}
+
+void ExponentialPacketGenerator::handleMessage(cMessage *msg) {
+    if (msg == event) { // received timing message
+        if (this->_packetsCount < this->_packetsNumber) {
+            this->generatedPacket = generatePacket(); // generate periodic packet
+
+            forwardPacket(this->generatedPacket); // send to random node
+
+            this->generatedPacket = NULL; // remove after send
+
+
+            simtime_t delay = getDelay();
+            scheduleAt(simTime() + delay, event);
+
+            std::string buf;
+            sprintf((char*) buf.c_str(), "Packet number %d generated with delay %lf", this->_packetsCount++, delay.dbl());
+            EV << buf.c_str();
+            bubble(buf.c_str());
+
+        }
+
+    } else { // received true packet
+        // processing received packet....
+        SimplePacket *sPacket = check_and_cast<SimplePacket *>(msg); // dynamic cast
+
+        if (sPacket->getDST() == getIndex()) {
+            EV << "Packet " << sPacket << " arrived\n";
+
+            delete sPacket;
+        } else {
+            forwardPacket(sPacket);
+        }
+
+    }
+}
+
+} /* namespace omnetpptrafficgenerators */
---
+++ src/OnOffPacketGenerator.h
@@ -0,0 +1,50 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef ONOFFPACKETGENERATOR_H_
+#define ONOFFPACKETGENERATOR_H_
+
+#include <PacketGenerator.h>
+#include <SimplePacket_m.h>
+#include <omnetpp.h>
+
+namespace omnetpptrafficgenerators {
+
+class OnOffPacketGenerator : public PacketGenerator  {
+public:
+    OnOffPacketGenerator();
+    virtual ~OnOffPacketGenerator();
+
+    virtual SimplePacket *generatePacket(); // overrided
+    virtual simtime_t getDelay();   // overrided
+
+protected:
+    virtual void handleMessage(cMessage *msg);  // overrided
+
+private:
+    void updateState();
+    enum State {
+        ON,
+        OFF,
+        NONE
+    };
+    State _state;
+    simtime_t _stateEndTime;
+};
+
+Define_Module(OnOffPacketGenerator);
+
+} /* namespace omnetpptrafficgenerators */
+#endif /* ONOFFPACKETGENERATOR_H_ */
